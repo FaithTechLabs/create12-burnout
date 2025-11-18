873bd0a77443c5032f47c1a8c21c8a879d47140e
Destin Bernie <berniedestin7@gmail.com>
2025-11-18 16:29:50 -0500

    Add logo, test generic header for survey, fix question formatting with quotes

--- app/helpers/survey_helper.rb
+++ app/helpers/survey_helper.rb
@@ -1,2 +1,9 @@
 module SurveyHelper
+  # Helper to embed an SVG file directly into the HTML
+  def inline_svg_tag(filename, options = {})
+    path = Rails.root.join('app', 'assets', 'images', filename)
+    return unless File.exist?(path)
+
+    File.read(path).html_safe
+  end
 end
